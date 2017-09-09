c1b8d5c6e7abe3ee3c36edff129319efd1f3dd40
Peter <peter@pkeogan.com>
2017-09-09 14:19:57 +0000

    First commit

---
+++ install.sh
@@ -0,0 +1,29 @@
+# Clear Existing Workspace
+rm README.md php.ini hello-world.php
+
+# Install PHP 7.1
+#!/bin/bash
+sudo add-apt-repository ppa:ondrej/php -y
+sudo apt-get update -y
+
+sudo apt-get install php7.1-curl php7.1-cli php7.1-dev php7.1-gd php7.1-intl php7.1-mcrypt php7.1-json php7.1-mysql php7.1-opcache php7.1-bcmath php7.1-mbstring php7.1-soap php7.1-xml php7.1-zip -y
+
+sudo mv /etc/apache2/envvars /etc/apache2/envvars.bak
+sudo apt-get remove libapache2-mod-php5 -y
+sudo apt-get install libapache2-mod-php7.1 -y
+sudo cp /etc/apache2/envvars.bak /etc/apache2/envvars
+
+sudo a2dismod php5
+sudo a2enmod php7.1
+
+sudo service apache2 restart
+
+# Install Laravel
+sudo composer self-update
+sudo composer create-project --prefer-dist laravel/laravel ./laravel
+shopt -s dotglob
+sudo mv laravel/* ./
+sudo rm -rf laravel
+sudo sed -i 's/DocumentRoot\ \/home\/ubuntu\/workspace/DocumentRoot\ \/home\/ubuntu\/workspace\/public/g' /etc/apache2/sites-enabled/001-cloud9.conf
+sudo composer update
+sudo chmod -R 777 storagebash
